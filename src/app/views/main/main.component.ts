7e5bf31fb5f6f99d7a5d2409407924fe03e6be96
Bruno Pena Baeta <brunopenabaeta@outlook.com>
2025-04-18 18:44:06 -0300

    Added test logout button

--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
 import { HeaderComponent } from '../../components/header/header.component';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'o-main',
@@ -9,4 +11,13 @@ import { SidebarComponent } from '../../components/sidebar/sidebar.component';
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
-export class MainComponent {}
+export class MainComponent {
+  service: AuthService = inject(AuthService);
+  router: Router = inject(Router);
+
+  async logout() {
+    await lastValueFrom(this.service.logout()).then(() => {
+      this.router.navigate(['/login']);
+    });
+  }
+}
