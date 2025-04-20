c3c6716ea89a72562dc0cab1e6ad4569e0c787d1
Bruno Pena Baeta <brunopenabaeta@outlook.com>
2025-04-20 01:03:56 -0300

    Complete OAuth flow

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,72 +2,59 @@ import { Routes } from '@angular/router';
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-  {
-    path: '',
-    loadComponent: () =>
-      import('./views/main/main.component').then((m) => m.MainComponent),
-    canActivate: [authGuard],
-    children: [
-      {
-        path: '',
-        loadComponent: () =>
-          import('./views/main/home/home.component').then(
-            (m) => m.HomeComponent
-          ),
-      },
-    ],
-  },
-  {
-    path: 'login',
-    loadComponent: () =>
-      import('./views/login-layout/login-layout.component').then(
-        (m) => m.LoginLayoutComponent
-      ),
-    children: [
-      {
-        path: '',
-        loadComponent: () =>
-          import('./views/login-layout/login/login.component').then(
-            (m) => m.LoginComponent
-          ),
-      },
-      {
-        path: 'register',
-        loadComponent: () =>
-          import('./views/login-layout/register/register.component').then(
-            (m) => m.RegisterComponent
-          ),
-      },
-      {
-        path: 'forgot-password',
-        loadComponent: () =>
-          import(
-            './views/login-layout/forgot-password/forgot-password.component'
-          ).then((m) => m.ForgotPasswordComponent),
-      },
-      {
-        path: 'reset-password/:token',
-        loadComponent: () =>
-          import(
-            './views/login-layout/reset-password/reset-password.component'
-          ).then((m) => m.ResetPasswordComponent),
-      },
-    ],
-  },
-  {
-    path: 'oauth-callback',
-    loadComponent: () =>
-      import('./views/oauth-callback/oauth-callback.component').then(
-        (m) => m.OAuthCallbackComponent
-      ),
-  },
-  {
-    path: 'error',
-    loadComponent: () =>
-      import('./views/error/error.component').then((m) => m.ErrorComponent),
-  },
-  {
-    path: '**',
-    redirectTo: 'error',
-  },
+	{
+		path: '',
+		loadComponent: () => import('./views/main/main.component').then(m => m.MainComponent),
+		canActivate: [authGuard],
+		children: [
+			{
+				path: '',
+				loadComponent: () => import('./views/main/home/home.component').then(m => m.HomeComponent),
+			},
+		],
+	},
+	{
+		path: 'login',
+		loadComponent: () => import('./views/login-layout/login-layout.component').then(m => m.LoginLayoutComponent),
+		children: [
+			{
+				path: '',
+				loadComponent: () => import('./views/login-layout/login/login.component').then(m => m.LoginComponent),
+			},
+			{
+				path: 'register',
+				loadComponent: () =>
+					import('./views/login-layout/register/register.component').then(m => m.RegisterComponent),
+			},
+			{
+				path: 'forgot-password',
+				loadComponent: () =>
+					import('./views/login-layout/forgot-password/forgot-password.component').then(
+						m => m.ForgotPasswordComponent,
+					),
+			},
+			{
+				path: 'reset-password/:token',
+				loadComponent: () =>
+					import('./views/login-layout/reset-password/reset-password.component').then(
+						m => m.ResetPasswordComponent,
+					),
+			},
+			{
+				path: 'oauth-callback',
+				loadComponent: () =>
+					import('./views/login-layout/oauth-callback/oauth-callback.component').then(
+						m => m.OAuthCallbackComponent,
+					),
+			},
+		],
+	},
+	{
+		path: 'error',
+		loadComponent: () => import('./views/error/error.component').then(m => m.ErrorComponent),
+	},
+	{
+		path: '**',
+		redirectTo: 'error',
+	},
 ];
--- src/app/views/oauth-callback/oauth-callback.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'o-oauth-callback',
-  imports: [],
-  templateUrl: './oauth-callback.component.html',
-  styleUrl: './oauth-callback.component.scss',
-})
-export class OAuthCallbackComponent {}
