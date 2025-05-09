306b4c9e22bc7f5fc7d62cb1134b80584c0672a9
BrunoPB <brunopenabaeta@outlook.com>
2025-05-09 15:44:42 -0300

    Add roadmap generation functionality and client classes

--- src/app/components/pop-ups/roadmap-creation-pop-up/roadmap-creation-pop-up.component.ts
+++ src/app/components/pop-ups/roadmap-creation-pop-up/roadmap-creation-pop-up.component.ts
@@ -3,9 +3,12 @@ import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angu
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatStepperModule } from '@angular/material/stepper';
+import { lastValueFrom } from 'rxjs';
 import { RoadmapTypeEnum } from '../../../enums/RoadmapType.enum';
+import { Roadmap } from '../../../models/Roadmap';
 import { Syllabus } from '../../../models/Syllabus';
 import { ContextService } from '../../../services/context.service';
+import { RoadmapService } from '../../../services/roadmap.service';
 import { SyllabusComponent } from '../../syllabus/syllabus.component';
 import { PopUpHeaderComponent } from '../pop-up-header/pop-up-header.component';
 
@@ -25,6 +28,7 @@ import { PopUpHeaderComponent } from '../pop-up-header/pop-up-header.component';
 export class RoadmapCreationPopUpComponent {
 	ctx: ContextService = inject(ContextService);
 	formBuilder: FormBuilder = inject(FormBuilder);
+	service: RoadmapService = inject(RoadmapService);
 
 	isLoading: boolean = false;
 
@@ -105,11 +109,77 @@ export class RoadmapCreationPopUpComponent {
 		this.getFormControl(1, 'type').setValue(type);
 	}
 
-	createRoadmap() {
+	async createRoadmap() {
 		if (this.forms.valid) {
 			this.isLoading = true;
-			// TODO: one call per type
-			// add a switch to identify which method to call
+
+			let endpoint: string = '';
+			let requestBody: any = {};
+			let syllabusIds: string[] = [];
+			this.getFormControl(0, 'syllabus').value.forEach((syllabus: Syllabus) => {
+				syllabusIds.push(syllabus.id);
+			});
+			let name: string = this.getFormControl(1, 'name').value;
+
+			switch (this.getFormControl(1, 'type').value) {
+				case RoadmapTypeEnum.VIDEO:
+					endpoint = 'video';
+					requestBody = {
+						syllabusIds,
+						name,
+						numberOfVideos: this.getFormControl(2, 'numberOfVideos').value,
+					};
+					break;
+				case RoadmapTypeEnum.TEXT:
+					endpoint = 'text';
+					requestBody = {
+						syllabusIds,
+						name,
+						numberOfParagraphs: this.getFormControl(2, 'numberOfParagraphs').value,
+						useTopics: this.getFormControl(2, 'useTopics').value,
+						formality: this.getFormControl(2, 'formality').value,
+					};
+					break;
+				case RoadmapTypeEnum.AUDIO:
+					endpoint = 'audio';
+					requestBody = {
+						syllabusIds,
+						name,
+						durationInSeconds: this.getFormControl(2, 'durationInSeconds').value,
+						formality: this.getFormControl(2, 'formality').value,
+					};
+					break;
+				case RoadmapTypeEnum.FLASH_CARD:
+					endpoint = 'flashcard';
+					requestBody = {
+						syllabusIds,
+						name,
+						numberOfCards: this.getFormControl(2, 'numberOfCards').value,
+						level: this.getFormControl(2, 'level').value,
+					};
+					break;
+				case RoadmapTypeEnum.QUESTION:
+					endpoint = 'question';
+					requestBody = {
+						syllabusIds,
+						name,
+						numberOfQuestions: this.getFormControl(2, 'numberOfQuestions').value,
+						level: this.getFormControl(2, 'level').value,
+						questionTypes: this.getFormControl(2, 'questionTypes').value,
+					};
+					break;
+			}
+
+			await lastValueFrom(this.service.generateRoadmap(requestBody, endpoint))
+				.then((r: Roadmap) => {
+					if (r) {
+						// TODO: Handle the response
+						console.log('Roadmap created successfully', r);
+					}
+				})
+				.finally(() => {
+					this.isLoading = false;
+				});
 		}
 	}
 }
--- src/app/services/roadmap.service.ts
+++ src/app/services/roadmap.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
-import { Roadmap } from '../models/Roadmap';
+import { AudioRoadmap, FlashCardRoadmap, QuestionRoadmap, Roadmap, VideoRoadmap } from '../models/Roadmap';
 
 @Injectable({
 	providedIn: 'root',
@@ -11,11 +11,19 @@ export class RoadmapService {
 	api: string = `${environment.API_URL}/roadmap`;
 	http: HttpClient = inject(HttpClient);
 
+	// GET
+
 	getUserRoadmapsByInstitution(institutionId: string): Observable<Roadmap[]> {
 		return this.http.get<Roadmap[]>(`${this.api}/${institutionId}`);
 	}
 
 	getClassroomSharedRoadmaps(classroomId: string): Observable<Roadmap[]> {
 		return this.http.get<Roadmap[]>(`${this.api}/classroom/${classroomId}`);
+	}
+
+	// GENERATION
+
+	generateRoadmap(requestBody: any, endpoint: string): Observable<Roadmap> {
+		return this.http.post<Roadmap>(`${this.api}/${endpoint}`, requestBody);
 	}
 }
