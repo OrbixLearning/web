55cb8f4dd5eccd24ecc75a6207c4ee7ee9cfb40e
Bruno Pena Baeta <brunopenabaeta@outlook.com>
2025-07-06 21:04:18 -0300

    Preparing environment for staging

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
 	ENV: 'prod',
-	API_URL: '',
-	OAUTH_REDIRECT_URI: '',
-	ACCOUNT_LINK_REDIRECT_URI: '',
+	API_URL: 'https://app.orbixlearning/api',
+	OAUTH_REDIRECT_URI: 'https://app.orbixlearning/login/oauth-callback',
+	ACCOUNT_LINK_REDIRECT_URI: 'https://app.orbixlearning/link-account-callback',
 	MAX_PDF_SIZE: 100000000, // 100 MB
 	MAX_IMAGE_SIZE: 10000000, // 10 MB
 };
