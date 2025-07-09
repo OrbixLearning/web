a5779a8d25c4b907f900b4ab4b4d4e8e95f9483d
BrunoPB <brunopenabaeta@outlook.com>
2025-07-09 18:29:08 -0300

    Connection frontend to backend in staging environment

--- src/environments/environment.hom.ts
+++ src/environments/environment.hom.ts
@@ -1,8 +1,8 @@
 export const environment = {
 	ENV: 'hom',
-	API_URL: 'https://app.orbixlearning/api',
-	OAUTH_REDIRECT_URI: 'https://app.orbixlearning/login/oauth-callback',
-	ACCOUNT_LINK_REDIRECT_URI: 'https://app.orbixlearning/link-account-callback',
+	API_URL: 'http://137.131.210.148/api',
+	OAUTH_REDIRECT_URI: 'http://137.131.210.148/login/oauth-callback',
+	ACCOUNT_LINK_REDIRECT_URI: 'http://137.131.210.148/link-account-callback',
 	MAX_PDF_SIZE: 100000000, // 100 MB
 	MAX_IMAGE_SIZE: 10000000, // 10 MB
 };
