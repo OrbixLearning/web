71753955a87dc2d7f8a610b084f6baf00ceb982d
Bruno Pena Baeta <brunopenabaeta@outlook.com>
2025-07-13 21:02:07 -0300

    Controller mapped to frontend service

---
+++ src/app/services/aichat.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { AIChatMessage } from '../models/AIChatMessage';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class AIChatService {
+	api: string = `${environment.API_URL}/chat`;
+	http: HttpClient = inject(HttpClient);
+
+	getChatHistory(classroomId: string, page: number, size: number): Observable<AIChatMessage[]> {
+		let params = new HttpParams().set('page', page).set('size', size);
+		return this.http.get<AIChatMessage[]>(`${this.api}/${classroomId}`, { params });
+	}
+
+	chat(classroomId: string, message: string): Observable<AIChatMessage> {
+		return this.http.post<AIChatMessage>(`${this.api}/${classroomId}`, { message });
+	}
+}
