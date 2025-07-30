9020d8e6fec8093ebb23a7c26cb3a03d9e16ea2a
BrunoPB <brunopenabaeta@outlook.com>
2025-07-30 15:15:46 -0300

    Partial commit: Dashboard view

--- src/app/views/main/classroom/classroom-dashboard/classroom-dashboard.component.ts
+++ src/app/views/main/classroom/classroom-dashboard/classroom-dashboard.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+import { LoadingComponent } from '../../../../components/loading/loading.component';
+import { ContextService } from '../../../../services/context.service';
 
 @Component({
-  selector: 'o-classroom-dashboard',
-  imports: [],
-  templateUrl: './classroom-dashboard.component.html',
-  styleUrl: './classroom-dashboard.component.scss'
+	selector: 'o-classroom-dashboard',
+	imports: [LoadingComponent, MatButtonModule, MatIconModule, RouterModule],
+	templateUrl: './classroom-dashboard.component.html',
+	styleUrl: './classroom-dashboard.component.scss',
 })
 export class ClassroomDashboardComponent {
+	ctx: ContextService = inject(ContextService);
 
+	isLoading = false;
 }
