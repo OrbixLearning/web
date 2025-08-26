a4c412e26f4e8cea623bfe28ddc29f3dc919712a
Bruno Pena Baeta <brunopenabaeta@outlook.com>
2025-08-26 11:47:54 -0300

    Added user home screen and improved routing

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,13 +2,14 @@ import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { Router, RouterModule } from '@angular/router';
+import { ActivatedRoute, Event, NavigationEnd, Router, RouterEvent, RouterModule } from '@angular/router';
 import { SelectModule } from 'primeng/select';
+import { filter } from 'rxjs';
 import { InstitutionRoleEnum } from '../../enums/InstitutionRole.enum';
 import { Institution } from '../../models/Institution';
 import { ContextService } from '../../services/context.service';
-import { ThemeService } from '../../services/theme.service';
 import { InstitutionService } from '../../services/institution.service';
+import { ThemeService } from '../../services/theme.service';
 import { UserService } from '../../services/user.service';
 
 @Component({
@@ -23,6 +24,7 @@ export class HeaderComponent {
 	theme: ThemeService = inject(ThemeService);
 	institutionService: InstitutionService = inject(InstitutionService);
 	userService: UserService = inject(UserService);
+	route: ActivatedRoute = inject(ActivatedRoute);
 
 	@Output() sidebar: EventEmitter<void> = new EventEmitter<void>();
 
@@ -35,17 +37,21 @@ export class HeaderComponent {
 	selectedInstitutionId: string | null = this.personalInstitution.id;
 
 	constructor() {
-		const urlSegments = this.router.url.split('/');
-		this.institutionId = urlSegments.length > 2 && urlSegments[1] === 'i' ? urlSegments[2] : undefined;
-		if (this.institutionId) this.selectedInstitutionId = this.ctx.institution!.id;
+		this.setInstitution();
 	}
 
 	ngOnInit() {
 		this.setThemes();
+		// This is used to update the data when the institutionId changes in the URL
+		this.router.events
+			.pipe(filter((e: Event | RouterEvent): e is RouterEvent => e instanceof NavigationEnd))
+			.subscribe((e: RouterEvent) => {
+				this.setInstitution();
+				this.setThemes();
+			});
 	}
 
 	get institutions(): Institution[] {
-		const institutionList = this.ctx.institutionList;
 		let arr: Institution[] = [this.personalInstitution];
 		arr.push(...(this.ctx.institutionList || []));
 		return arr;
@@ -69,6 +75,12 @@ export class HeaderComponent {
 		return this.userService.getProfilePictureUrl(this.ctx.user!);
 	}
 
+	setInstitution() {
+		const urlSegments = this.router.url.split('/');
+		this.institutionId = urlSegments.length > 2 && urlSegments[1] === 'i' ? urlSegments[2] : undefined;
+		if (this.institutionId) this.selectedInstitutionId = this.ctx.institution!.id;
+	}
+
 	setThemes() {
 		if (this.ctx.institution?.id) this.theme.setInstitutionTheme(this.ctx.institution);
 		else this.theme.setBaseTheme();
@@ -76,7 +88,6 @@ export class HeaderComponent {
 
 	changeInstitution(institutionId: string | null) {
 		this.ctx.institution = this.institutions.find(inst => inst.id === institutionId) || this.personalInstitution;
-		this.setThemes();
 		if (institutionId) {
 			this.router.navigate(['/i/' + institutionId]);
 		} else {
