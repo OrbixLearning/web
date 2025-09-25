33f9ee387e50c79844374585e5fc66eba0e07aad
Ranmdom <slimegamer12@gmail.com>
2025-09-24 21:08:03 -0300

    Subindo criação de rota

--- src/app/components/pop-ups/learning-path-creation-pop-up/learning-path-creation-pop-up.component.spec.ts
+++ src/app/components/pop-ups/learning-path-creation-pop-up/learning-path-creation-pop-up.component.spec.ts
@@ -1,22 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LearningPathCreationPopUpComponent } from './learning-path-creation-pop-up.component';
 
 describe('LearningPathCreationPopUpComponent', () => {
-	let component: LearningPathCreationPopUpComponent;
-	let fixture: ComponentFixture<LearningPathCreationPopUpComponent>;
+  let component: LearningPathCreationPopUpComponent;
+  let fixture: ComponentFixture<LearningPathCreationPopUpComponent>;
 
-	beforeEach(async () => {
-		await TestBed.configureTestingModule({
-			imports: [LearningPathCreationPopUpComponent],
-		}).compileComponents();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [LearningPathCreationPopUpComponent],
+    }).compileComponents();
 
-		fixture = TestBed.createComponent(LearningPathCreationPopUpComponent);
-		component = fixture.componentInstance;
-		fixture.detectChanges();
-	});
+    fixture = TestBed.createComponent(LearningPathCreationPopUpComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-	it('should create', () => {
-		expect(component).toBeTruthy();
-	});
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
