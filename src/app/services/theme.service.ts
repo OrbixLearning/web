e67a15de42e41fb3f89afaa5b07dd0f228e26817
Bruno Pena Baeta <brunopenabaeta@outlook.com>
2025-09-27 23:03:52 -0300

    Improvement to white-label

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -8,52 +8,47 @@ export class ThemeService {
 	private themeStyleElementId = 'institution-theme-style';
 
 	private clearThemes() {
-		document.body.classList.forEach(className => {
-			if (className.startsWith('theme-')) {
-				document.body.classList.remove(className);
-			}
-		});
-
-		const styleElement = document.getElementById(this.themeStyleElementId);
-		if (styleElement) {
-			styleElement.remove();
-		}
+		const root = document.documentElement;
+		root.removeAttribute('data-theme');
+		root.removeAttribute('style');
 	}
 
-	private buildCssClass(themeClass: string, style: InstitutionStyle): string {
-		return `.${themeClass} {
-        ${style.primaryColor ? `--primary-color: ${style.primaryColor};` : ''}
-        ${style.secondaryColor ? `--secondary-color: ${style.secondaryColor};` : ''}
-        ${style.backgroundColor ? `--background-color: ${style.backgroundColor};` : ''}
-        ${style.textColor ? `--text-color: ${style.textColor};` : ''}
-        ${style.theme ? `--theme: ${style.theme};` : ''}
-        }`;
+	private buildStyleTokens(style: InstitutionStyle): Record<string, string> {
+		return {
+			'primary-color': style.primaryColor,
+			'secondary-color': style.secondaryColor,
+			'background-color': style.backgroundColor,
+			'text-color': style.textColor,
+			'theme': style.theme,
+		};
 	}
 
 	setInstitutionTheme(institution: Institution) {
 		this.clearThemes();
 
 		const themeClass: string = `theme-inst-${institution.id}`;
-		document.body.classList.add(themeClass);
+		const root = document.documentElement;
+		root.setAttribute('data-theme', themeClass);
+		const styleTokens = this.buildStyleTokens(institution.style!);
 
-		const styleEl = document.createElement('style');
-		styleEl.id = this.themeStyleElementId;
-		styleEl.innerText = this.buildCssClass(themeClass, institution.style!);
-		document.head.appendChild(styleEl);
-	}
-
-	setBaseTheme() {
-		this.clearThemes();
-		document.body.classList.add('theme-base');
+		Object.entries(styleTokens).forEach(([k, v]) => {
+			root.style.setProperty(`--${k}`, v);
+		});
 	}
 
 	setDarkTheme() {
 		this.clearThemes();
-		document.body.classList.add('theme-dark');
+		const root = document.documentElement;
+		root.setAttribute('data-theme', 'dark');
 	}
 
 	setLightTheme() {
 		this.clearThemes();
-		document.body.classList.add('theme-light');
+		const root = document.documentElement;
+		root.setAttribute('data-theme', 'light');
+	}
+
+	setBaseTheme() {
+		this.clearThemes();
 	}
 }
