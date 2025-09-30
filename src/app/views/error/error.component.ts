575ec2e8b2954af6ce8b9e618e52a897cfa1a17d
Bruno Pena Baeta <brunopenabaeta@outlook.com>
2025-09-30 16:08:26 -0300

    Error page styling

--- src/app/views/error/error.component.ts
+++ src/app/views/error/error.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
+import { TextButtonComponent } from '../../components/buttons/text-button/text-button.component';
 
 @Component({
-  selector: 'o-error',
-  imports: [],
-  templateUrl: './error.component.html',
-  styleUrl: './error.component.scss',
+	selector: 'o-error',
+	imports: [MatIconModule, TextButtonComponent],
+	templateUrl: './error.component.html',
+	styleUrl: './error.component.scss',
 })
-export class ErrorComponent {}
+export class ErrorComponent {
+	router: Router = inject(Router);
+
+	goBack() {
+		window.history.back();
+	}
+
+	goHome() {
+		this.router.navigate(['/']);
+	}
+
+	openReport() {
+		this.router.navigate(['/report']);
+	}
+
+	reload() {
+		window.location.reload();
+	}
+}
