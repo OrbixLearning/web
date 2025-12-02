5033bcdcf9bfbdadd6d8a0fa497d0724d164fd65
Bruno Pena Baeta <brunopenabaeta@outlook.com>
2025-12-02 13:03:23 -0300

    LMS layout

--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../../components/header/header.component';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { ContextService } from '../../services/context.service';
 @Component({
 	selector: 'o-main',
 	imports: [HeaderComponent, SidebarComponent, RouterOutlet, MatSidenavModule],
 	templateUrl: './main.component.html',
 	styleUrl: './main.component.scss',
 })
-export class MainComponent {}
+export class MainComponent {
+	ctx: ContextService = inject(ContextService);
+
+	get inLMS(): boolean {
+		return this.ctx.inLMS;
+	}
+}
