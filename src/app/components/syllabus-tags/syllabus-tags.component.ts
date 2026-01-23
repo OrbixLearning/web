04f8e0edf0f00ea4a13116133b884cd496d980dc
Bruno Pena Baeta <brunopenabaeta@outlook.com>
2026-01-23 00:59:15 -0300

    Improvement to syllabus tags component

--- src/app/components/syllabus-tags/syllabus-tags.component.ts
+++ src/app/components/syllabus-tags/syllabus-tags.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { TagModule } from 'primeng/tag';
+import { TooltipModule } from 'primeng/tooltip';
 import { Syllabus } from '../../models/Syllabus';
 
 @Component({
 	selector: 'o-syllabus-tags',
-	imports: [TagModule],
+	imports: [TagModule, TooltipModule],
 	templateUrl: './syllabus-tags.component.html',
 	styleUrl: './syllabus-tags.component.scss',
 })
